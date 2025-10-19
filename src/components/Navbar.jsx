c1c24dc2204143c7e5e3a8a6423369e14e0934cd
Thakur Rajeshwar <sunnythakur13919@gmail.com>
2025-10-19 18:49:17 +0530

    categogy added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
                 <NavLink to='/'>Home</NavLink>
-                <NavLink to='/products'>AllProducts</NavLink>
+                <NavLink to='/products'>All Products</NavLink>
                 <NavLink to='/contacts'>Contacts</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
