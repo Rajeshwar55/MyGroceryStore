b78e6c04a2dca1292a3af8a4ca0aa9465c12d973
Thakur Rajeshwar <sunnythakur13919@gmail.com>
2025-10-26 23:26:36 +0530

    cart added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,11 +50,17 @@ const Navbar = () => {
                  </div>
                  )}
             </div>
+            <div className='flex items-center gap-6 sm:hidden'>
+                 <div className="relative cursor-pointer">
+                    <img onClick={()=>navigate('/cart')} src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-60' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary-dull w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
                 {/* Menu Icon SVG */}
               <img src={assets.menu_icon} alt="menu" />
             </button>
+            </div>
 
             {user &&
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
